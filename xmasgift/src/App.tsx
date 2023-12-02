95f214ef7b2b13a0fe522c2a71d8da5f98c3ef8c
Christian Polli <74564883+Maracujacake@users.noreply.github.com>
2023-12-02 02:19:47 +0000

    0.2.0

--- xmasgift/src/App.tsx
+++ xmasgift/src/App.tsx
@@ -1,12 +1,42 @@
 // src/App.tsx
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const glitterAnimation = keyframes`
+  0% {
+    background-position: 0% 0%;
+  }
+  100% {
+    background-position: 400% 400%;
+  }
+`;
+
+const GlitterText = styled.h1`
+  font-size: 2em;
+  position: relative;
+  display: inline-block;
+  font-weight: bold;
+  color: gray;
+  -webkit-background-clip: text;
+  background-clip: text;
+  animation: ${glitterAnimation} 2s linear infinite;
+`;
+
+const Container = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <img src="./hello-kitty-merry-xmas.gif" alt="Hello Kitty GIF" />
-      <h1 className="glitter-text">Feliz Natal!</h1>
-    </div>
+    <Container>
+      <img src="/hello-kitty-merry-xmas.gif" alt="Hello Kitty GIF" />
+      <div className="text-container">
+        <GlitterText>Feliz Natal!</GlitterText>
+      </div>
+    </Container>
   );
 };
 
