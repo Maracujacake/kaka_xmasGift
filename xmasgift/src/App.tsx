ac0dcb0f53135553da45bc998ca88c28ce48b213
Christian Polli <74564883+Maracujacake@users.noreply.github.com>
2023-12-02 02:04:38 +0000

    0.1.0

---
+++ xmasgift/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <img src="./hello-kitty-merry-xmas.gif" alt="Hello Kitty GIF" />
+      <h1 className="glitter-text">Feliz Natal!</h1>
+    </div>
+  );
+};
+
+export default App;
